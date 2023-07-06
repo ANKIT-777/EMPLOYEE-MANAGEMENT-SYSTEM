56371fcdf6bc9c298fd89c5b72ebe819784e03d4
Ankit Sharma <an2ki4t5s@gmail.com>
2023-07-06 20:12:14 +0530

    isntalled package files

---
+++ models/employee.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const employeeSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  designation: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Employee', employeeSchema);
